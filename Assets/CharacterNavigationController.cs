d884a1541085fd2992ec3708badb32ef390353e8
Mateo <mateoandraca@hotmail.com>
2022-05-25 20:15:14 -0300

    V-0.1.19 Pre-Alpha(fixed)

--- Assets/CharacterNavigationController.cs
+++ Assets/CharacterNavigationController.cs
@@ -2,17 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class CharacterNavigationController : MonoBehaviour
 {
+
+    public int movementSpeed = 1;
+    public int rotationSpeed = 120;
+    public float stopDistance = 2.5f;
+    public Vector3 destination;
+    public bool reachedDestination;
+    Vector3 velocity;
+    Vector3 lastPosition;
 
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        
+        if (transform.position != destination)
+        {
+            Vector3 destinationDirection = destination - transform.position;
+            destinationDirection.y = 0;
+
+            float destinationDistance = destinationDirection.magnitude;
+
+            if (destinationDistance >= stopDistance)
+            {
+                reachedDestination = false;
+                Quaternion targetRotation = Quaternion.LookRotation(destinationDirection);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            }
+            else
+            {
+                reachedDestination = true;
+            }
+
+            velocity = (transform.position - lastPosition) / Time.deltaTime;
+            velocity.y = 0;
+            var velocityMagnitude = velocity.magnitude;
+            velocity = velocity.normalized;
+            var fwdDotProduct = Vector3.Dot(transform.forward, velocity);
+            var rightDotProduct = Vector3.Dot(transform.right, velocity);
+
+            //animator.SetFloat("Horiontal",rightDotProduct);
+            //animator.SetFloat("Forward",fwdDotProduct);
+        }
+        else
+        {
+            reachedDestination = true;
+        }
     }
 
     public void SetDestination(Vector3 destination)
