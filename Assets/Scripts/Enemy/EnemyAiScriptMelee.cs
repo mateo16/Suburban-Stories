8cd201ce873f40b02bf4b79b45e478737d463c49
AlanMutz <alanmutzmajer@gmail.com>
2022-05-25 21:19:08 -0300

    V-0.1.20 Pre-Alpha
    Chabon hace jueguitos con el cuchi

---
+++ Assets/Scripts/Enemy/EnemyAiScriptMelee.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAiScriptMelee : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform player;
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    public MeleeSystemEnemy MeleeSystemEnemyScript;
+
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    public float timebetweenAttacks;
+    bool alreadyAttacked;
+
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+    private void Update()
+    {
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!playerInSightRange && !playerInAttackRange) Patroling();
+        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if (playerInSightRange && playerInAttackRange) AttackPlayer();
+    }
+    private void Patroling()
+    {
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
+            agent.SetDestination(walkPoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+    }
+    private void SearchWalkPoint()
+    {
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+            walkPointSet = true;
+    }
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+    private void AttackPlayer()
+    {
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+        if (!alreadyAttacked)
+        {
+            MeleeSystemEnemyScript.SwordAttack();
+        }
+    }
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
+}
---
+++ Assets/Scripts/MeleeSystemEnemy.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeSystemEnemy : MonoBehaviour
+{
+    public GameObject Sword;
+    public bool CanAttack = true;
+    public float AttackCooldown = 0.5f;
+    public int damage = 1;
+    public bool isAttacking = false;
+
+    public GameObject enemyImpact;
+    public Transform bloodPoint;
+
+    public void SwordAttack()
+    {
+        isAttacking = true;
+        CanAttack = false;
+        Animator anim = Sword.GetComponent<Animator>();
+        anim.SetTrigger("Attack");
+        Invoke("ResetAttack", AttackCooldown);
+    }
+
+    private void ResetAttack()
+    {
+        CanAttack = true;
+        isAttacking = false;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Enemy" && isAttacking)
+        {
+            other.GetComponent<EnemyDamage>().TakeDamage(damage);
+            Instantiate(enemyImpact, bloodPoint.position, bloodPoint.rotation);
+            isAttacking = false;
+        }
+    }
+}
