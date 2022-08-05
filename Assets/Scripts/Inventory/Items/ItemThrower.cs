5d068b1ab2b4ad44a4768a1f648fb2c18051ec55
mateo16 <50384944+mateo16@users.noreply.github.com>
2022-08-05 11:58:15 -0300

    V-0.2.23 Pre-Alpha
    Se pueden tirar cosas y esta todo mas ordenado

--- Assets/Scripts/Inventory/Items/ItemThrower.cs
+++ Assets/Scripts/Inventory/Items/ItemThrower.cs
@@ -7,6 +7,11 @@ namespace DapperDino.Items
     public class ItemThrower : MonoBehaviour
     {
         [SerializeField] private Inventory inventory = null;
+        [SerializeField] private GameObject[] pickUpPrefabs;
+        [SerializeField] private Transform fpsCam,player,firePoint;
+        [SerializeField] private float dropForwardForce, dropUpwardForce;
+        [SerializeField] private HandShow hand;
+        private GameObject selectedPrefab;
 
         private int slotIndex = 0;
 
@@ -14,11 +19,32 @@ namespace DapperDino.Items
 
         public void Activate(ItemSlot itemSlot, int slotIndex)
         {
+
+        foreach (var prefab in pickUpPrefabs)
+        {
+            if (prefab.name == itemSlot.item.ColouredName)
+            {
+                selectedPrefab = prefab;
+            }
+        }
+
+        if(selectedPrefab != null)
+        {
+             Rigidbody rb = Instantiate(selectedPrefab, firePoint.position, Quaternion.identity).GetComponent<Rigidbody>();
+
+            rb.velocity = player.GetComponent<Rigidbody>().velocity;
+            rb.AddForce(fpsCam.forward * dropForwardForce , ForceMode.Impulse);
+            rb.AddForce(fpsCam.up * dropUpwardForce , ForceMode.Impulse);
+            
+            float random = Random.Range(-1f,1f);
+            rb.AddTorque(new Vector3(random,random,random)*10);
+            }
             this.slotIndex = slotIndex;
 
             inventory.RemoveAt(slotIndex);
-
-            //Instantiate item
+            
+            hand.ChangePrefab("");
+            selectedPrefab = null;
         }
     }
 }
